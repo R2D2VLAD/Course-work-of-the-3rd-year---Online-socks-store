83c5d94329ee4dd1840e837bbf2a854f90282de9
Vlad <zitarvlad11@gmail.com>
2023-02-06 12:32:28 +0600

    Course-work-of-the-3rd-year---Online-socks-store

---
+++ src/main/java/com/example/onlinesocksstore/CourseWorkOfThe3rdYearOnlineSocksStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinesocksstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkOfThe3rdYearOnlineSocksStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkOfThe3rdYearOnlineSocksStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlinesocksstore/CourseWorkOfThe3rdYearOnlineSocksStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinesocksstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkOfThe3rdYearOnlineSocksStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
